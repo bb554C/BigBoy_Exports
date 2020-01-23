af5828ee3b083d6debf62df8ee66054ca7c05298
bb554C <bairdbuenaventura@gmail.com>
2020-01-23 18:12:17 +0800

    Initial GUI with Tabbed Panes

--- src/HomePage.java
+++ src/HomePage.java
@@ -4,10 +4,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 
 public class HomePage extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table1;
 
 	/**
 	 * Launch the application.
@@ -29,12 +32,21 @@ public class HomePage extends JFrame {
 	 * Create the frame.
 	 */
 	public HomePage() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(0, 0, 600, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+		
+		JTabbedPane tab1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("Main Tab", null, tab1, null);
+		
+		table1 = new JTable();
+		tabbedPane.addTab("Data Tab", null, table1, null);
 	}
-
 }
